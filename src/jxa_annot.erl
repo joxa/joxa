10484207af118e0f623bb8b14529abd0ef3c65ff
Eric Merritt <ericbmerritt@gmail.com>
2011-12-17 13:05:24 -0500

    simplify pathing for AST annotation
    Breaks annotations and pathing into to seperate modules jxa_path and
    jxa_annot and vastly simplifies how pathing is done (moving to
    position pathing instead of hash based pathing). It also moves pathing
    manipulation to a direct state based approach that should be more
    intuitive and hopefully easier to understand.

--- src/jxa_annot.erl
+++ src/jxa_annot.erl
@@ -1,69 +1,16 @@
 %% Path Hashing for Line and Type Information
 %% ==========================================
-
-%% We have the problem that for macros and for ease of parsing we want
-%% to keep the AST as clean as possible. That is, we want it to be as
-%% close to a normal list as we can get away with. However, we want to
-%% be able to hang information on the graph that the AST
-%% represents. Things like line numbers, additional type information
-%% etc. However, in erlang we cant do that without polluting the graph
-%% itself and making it harder for user written macros to be
-%% implemented. So we need some way to identify specific points in the
-%% graph that is the AST that we can then use as a key on this
-%% additional information that we would like to show.
-%%
-%% In an AST nodes are identified by their location in the graph. That
-%% is, every node in the graph is identified by the path leading to
-%% that node.  Lets look at an example.
-%%
-%%     (hello world (I rock))
-%%
-%% In this case the 'I' node could be identified by the path [hello,
-%% world, <start of children>, I]. This should be a unique identifier
-%% for any point in the graph assuming that there is a single root
-%% term being parsed.
-%%
-%% If that is true we can replace the actual elements with their
-%% positions in the list. So the example above would become. [1, 3,
-%% 1]. Where the outer list is 1 (everything starts at one) the 3rd
-%% position is the list and the first element in that third
-%% position. Lets look at something a bit more something more realistic.
-%%
-%%     (defn+ hello-world [foo bar] (baz bang bong))
-%%
-%% In this example the bang node could be identified by everything
-%% leading up to it. So the path would be [defn+, hello-world,
-%% <children>, <start-of_children>, bang]. Lets translate this to our
-%% simple numerical paths. [1, 4, 2]. This should work to any level in
-%% the graph.
-%%
-%% We can make it even easier to manipulate buy having the firstest
-%% point in the graph be the closest point in the list so that we can
-%% push onto the list as we go forward. The actual path in the example
-%% above would be [2, 4, 1] and built up each time we see a list.
 -module(jxa_annot).
 
 -export([new/0,
-         new_path/0,
-         new_base_position/2,
-         add_base_position/2,
-         add/2,
-         add_annot/3,
-         get_annot/2]).
-
--define(START_CHILDREN_MARKER, -100).
-
-
--define(PATH_ROOT, -1).
-
+         add/3,
+         get/2]).
 
 %%=============================================================================
 %% Types
 %%=============================================================================
 -type annotations() ::
-        ec_dictionary:dictionary(path(), Annotation::term()).
-
--type path() :: integer().
+        ec_dictionary:dictionary(jxa_path:path(), Annotation::term()).
 
 %%=============================================================================
 %% Internal Functions
@@ -78,41 +25,20 @@
 new() ->
     ec_dictionary:new(ec_dict).
 
-%% #### new_path An empty path is simply a list. This function exists
-%% more for documentation then anything else.
--spec new_path() -> path().
-new_path() ->
-   {0, []}.
-
--spec new_base_position(non_neg_integer(), path()) -> path().
-new_base_position(Pos, {_, Path}) ->
-    {Pos, Path}.
-
--spec add_base_position(non_neg_integer(), path()) -> path().
-add_base_position(Pos, {OldPos, Path}) ->
-    {Pos + OldPos, Path}.
-
-%% #### add
-%%
-%% Add a new position to the path.
--spec add(non_neg_integer(), path()) -> path().
-add(Position, {OldPosition, Path}) ->
-    {OldPosition, [Position + OldPosition | Path]}.
-
 %% #### add
 %%
 %% Given a pre created path, an annotation and the annotations object
 %% we add the annotation at that path.
--spec add_annot(path(), term(), annotations()) -> annotations().
-add_annot({_, Path}, Annots, Annotations) ->
+-spec add(jxa_path:path(), term(), annotations()) -> annotations().
+add(Path, Annots, Annotations) ->
     ec_dictionary:add(Path, Annots, Annotations).
 
 %% #### get
 %%
 %% Given a precreated path and an annotations object we get the
 %% annotation at that location.
--spec get_annot(path(), annotations()) -> term().
-get_annot({_, Path}, Annotations) ->
+-spec get(jxa_path:path(), annotations()) -> term().
+get(Path, Annotations) ->
     ec_dictionary:get(Path, Annotations).
 
 %%=============================================================================
--- src/jxa_literal.erl
+++ src/jxa_literal.erl
@@ -10,12 +10,14 @@
 %%=============================================================================
 %% Public API
 %%=============================================================================
--spec comp(jxa_annot:path(), jxa_ctx:context(), term()) -> cerl:cerl().
+-spec comp(jxa_path:state(), jxa_ctx:context(), term()) -> cerl:cerl().
 comp(Path0, Ctx0, Symbol) when is_atom(Symbol) ->
-    {_, {Line, _}} = jxa_annot:get_annot(Path0, jxa_ctx:annots(Ctx0)),
+    {_, {Line, _}} = jxa_annot:get(jxa_path:path(Path0),
+                                   jxa_ctx:annots(Ctx0)),
     cerl:ann_c_atom([Line], Symbol);
 comp(Path0, Ctx0, Integer) when is_integer(Integer) ->
-    {Type, {Line, _}} = jxa_annot:get_annot(Path0, jxa_ctx:annots(Ctx0)),
+    {Type, {Line, _}} = jxa_annot:get(jxa_path:path(Path0),
+                                      jxa_ctx:annots(Ctx0)),
     case Type of
         integer ->
             cerl:ann_c_int([Line], Integer);
@@ -23,10 +25,12 @@ comp(Path0, Ctx0, Integer) when is_integer(Integer) ->
             cerl:ann_c_char([Line], Integer)
     end;
 comp(Path0, Ctx0, Float) when is_float(Float) ->
-    {float, {Line, _}} = jxa_annot:get_annot(Path0, jxa_ctx:annots(Ctx0)),
+    {float, {Line, _}} = jxa_annot:get(jxa_path:path(Path0),
+                                       jxa_ctx:annots(Ctx0)),
     cerl:ann_c_float([Line], float);
 comp(Path0, Ctx0, Element) when is_list(Element) ->
-    {Type, {Line, _}} = jxa_annot:get_annot(Path0, jxa_ctx:annots(Ctx0)),
+    {Type, {Line, _}} = jxa_annot:get(jxa_path:path(Path0),
+                                      jxa_ctx:annots(Ctx0)),
     case Type of
         list ->
             comp_list(Path0, Line, Ctx0, Element);
@@ -39,27 +43,31 @@ comp(Path0, Ctx0, Element) when is_list(Element) ->
 %%=============================================================================
 %% Internal Functions
 %%=============================================================================
--spec comp_list(jxa_annot:path(), non_neg_integer(), jxa_ctx:context(), list()) ->
+-spec comp_list(jxa_path:state(), non_neg_integer(),
+                jxa_ctx:context(), list()) ->
                        cerl:cerl().
 comp_list(_Path0, _Line, _Ctx0, []) ->
     cerl:c_nil();
 comp_list(Path0, Line, Ctx0, [H | T]) ->
-    Path1 = jxa_annot:add_base_position(1, Path0),
-    cerl:ann_c_cons([Line], comp(Path0, Ctx0, H), comp_list(Path1, Line, Ctx0, T)).
+    cerl:ann_c_cons([Line], comp(jxa_path:add(Path0), Ctx0, H),
+                    comp_list(jxa_path:incr(Path0), Line, Ctx0, T)).
 
--spec comp_vector(jxa_annot:path(), non_neg_integer(), jxa_ctx:context(), list()) ->
+-spec comp_vector(jxa_path:state(), non_neg_integer(),
+                  jxa_ctx:context(), list()) ->
                          cerl:cerl().
 comp_vector(Path0, Line, Ctx0, Elements0) ->
     {_, Elements1} =
-        lists:foldl(fun(Element, {Count0, Acc0}) ->
-                            Count1 = Count0 + 1,
-                            Acc1 = [comp(jxa_annot:add(Count1, Path0), Ctx0, Element) | Acc0],
-                            {Count1, Acc1}
-                    end, {0, []}, Elements0),
+        lists:foldl(fun(Element, {Path1, Acc0}) ->
+                            Acc1 = [comp(jxa_path:add(Path1), Ctx0, Element)
+                                    | Acc0],
+                            Path2 = jxa_path:incr(Path1),
+                            {Path2, Acc1}
+                    end, {Path0, []}, Elements0),
     cerl:ann_c_tuple([Line],
                      Elements1).
 
--spec comp_string(jxa_annot:path(), non_neg_integer(), jxa_ctx:context(), list()) ->
+-spec comp_string(jxa_path:state(), non_neg_integer(),
+                  jxa_ctx:context(), list()) ->
                          cerl:cerl().
 comp_string(_Path0, Line, _Ctx0, String) ->
     cerl:ann_c_string([Line], String).
