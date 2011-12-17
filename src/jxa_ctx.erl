e026c07b698b0c74eb55d11cd90e7997a85052d6
Eric Merritt <ericbmerritt@gmail.com>
2011-12-17 12:46:41 -0500

    add variable lookup and scoping support to the context

--- src/jxa_ctx.erl
+++ src/jxa_ctx.erl
@@ -25,7 +25,12 @@
          line/2,
          definitions/1,
          add_exported_definition/5,
-         add_definition/5]).
+         add_definition/5,
+         resolve_variable/3,
+         push_scope/1,
+         add_variable_to_scope/2,
+         add_variables_to_scope/2,
+         pop_scope/1]).
 
 -export_type([context/0,
               attr/0,
@@ -42,8 +47,14 @@
                   alias :: alias(),
                   require :: require(),
                   use :: use(),
+                  scopes :: [set()],
                   definitions :: definition(),
                   line :: non_neg_integer()}).
+
+-define(FUN_SPLIT_PATTERN,
+        {re_pattern,0,0,
+         <<69,82,67,80,57,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,58,0,0,0,48,0,0,0,
+           0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,93,0,5,27,58,84,0,5,0>>}).
 
 %%=============================================================================
 %% Types
@@ -51,11 +62,15 @@
 -type context() :: record(context).
 -type attr() :: {Key::cerl:cerl(), Value::cerl:cerl()}.
 -type alias() :: ec_dictionary:dictionary(module(), module()).
--type require() :: set().
+-type require() :: ec_dictionary:dictionary(module(),
+                                            [{FunName::atom(),
+                                              Arity::non_neg_integer()}]).
 -type use_key() :: {FunctionName::atom(), arity()}.
 -type use_value() :: {FunctionName::atom(), ModuleName::module()}.
 -type use() :: ec_dictionary:dictionary(use_key(), use_value()).
--type definition() :: {cerl:c_fname(), cerl:c_fun()}.
+-type definition() :: ec_dictionary:dictionary(use_key(),
+                                               {cerl:c_fname(),
+                                                cerl:c_fun()}).
 -type export() :: {Fun::atom(), Arity::non_neg_integer(),
                    Line::non_neg_integer()}.
 %%=============================================================================
@@ -69,10 +84,11 @@ new(Annots) ->
     #context{module_name=undefined,
              attrs=[],
              exports=[],
-             definitions=[],
              annots=Annots,
+             scopes=[],
+             definitions=ec_dictionary:new(ec_dict),
              alias=ec_dictionary:new(ec_dict),
-             require=sets:new(),
+             require=ec_dictionary:new(ec_dict),
              use=ec_dictionary:new(ec_dict)}.
 
 -spec new(jxa_annot:annotations(),
@@ -84,9 +100,10 @@ new(Annots, ModuleName, Line)
              annots=Annots,
              exports=[],
              attrs=[],
-             definitions=[],
+             scopes=[],
+             definitions=ec_dictionary:new(ec_dict),
              alias=ec_dictionary:new(ec_dict),
-             require=sets:new(),
+             require=ec_dictionary:new(ec_dict),
              use=ec_dictionary:new(ec_dict)}.
 
 %% create a new context with default values for aliased modules, required
@@ -114,13 +131,19 @@ new(Annots, ModuleName, Line)
                   [module()], [{use_key(), use_value()}]) ->
                          context().
 new(Annots, Aliases, Requires, Uses) ->
+    Req0 = ec_dictionary:new(ec_dict),
+    Req2 = lists:foldl(fun(ModuleName, Req1) ->
+                               Exports = ModuleName:module_info(exports),
+                               ec_dictionary:add(ModuleName, Exports, Req1)
+                       end, Req0, Requires),
     #context{module_name=undefined,
              annots=Annots,
              exports=[],
              attrs=[],
-             definitions=[],
+             scopes=[],
+             definitions=ec_dictionary:new(ec_dict),
              alias=ec_dictionary:from_list(ec_dict, Aliases),
-             require=sets:from_list(Requires),
+             require=Req2,
              use=ec_dictionary:from_list(ec_dict, Uses)}.
 
 -spec annots(context()) -> jxa_annot:annotations().
@@ -186,7 +209,8 @@ require(Require, Ctx0) ->
 
 -spec add_require(module(), context()) -> context().
 add_require(Module, Ctx0=#context{require=Require}) ->
-    Ctx0#context{require=sets:add_element(Module, Require)}.
+    Exports = Module:module_info(exports),
+    Ctx0#context{require=ec_dictionary:add(Module, Exports, Require)}.
 
 -spec use(context()) -> use().
 use(#context{use=Use}) ->
@@ -202,7 +226,7 @@ use(Use, Ctx0) ->
 add_use(Alias, Arity, Target, Module, Ctx0=#context{use=Use}) ->
     Ctx0#context{use=ec_dictionary:add({Alias, Arity}, {Target, Module}, Use)}.
 
--spec definitions(context()) -> [definition()].
+-spec definitions(context()) -> definition().
 definitions(#context{definitions=Definitions}) ->
     Definitions.
 
@@ -224,5 +248,79 @@ add_definition(Line, Name, Vars, Body, Ctx0=#context{definitions=Defs}) ->
     CerlName = cerl:ann_c_fname([Line],
                                 Name, Arity),
     CerlBody = cerl:ann_c_fun([Line], Vars, Body),
-    Ctx0#context{definitions=[{CerlName, CerlBody} | Defs]}.
+    Ctx0#context{definitions=ec_dictionary:add({Name, Arity},
+                                               {CerlName, CerlBody}, Defs)}.
 
+
+resolve_variable(Name, PossibleArity, Ctx = #context{scopes=Scopes})
+  when is_atom(Name) ->
+    case lists:any(fun(Scope) ->
+                           sets:is_element(Name, Scope)
+                   end, Scopes) of
+        true ->
+            variable;
+        false ->
+            resolve_function_name(Name, PossibleArity, Ctx)
+    end.
+
+resolve_function_name(Name, PossibleArity, Ctx) ->
+    StringName = erlang:atom_to_list(Name),
+    case re:split(StringName, ?FUN_SPLIT_PATTERN) of
+        [_] ->
+            %% It must be in the define list already
+            search_for_defined_used_function(Name, PossibleArity, Ctx);
+        [Module, Function] ->
+            %% Check the requires and and make sure that that function and
+            %% Arity exist already.
+            search_for_remote_function(Module, Function, PossibleArity, Ctx);
+        _ ->
+            %% Invalid
+            invalid
+    end.
+
+search_for_remote_function(Module, Function, PossibleArity,
+                           #context{require=Requires}) ->
+    AModule = list_to_atom(binary_to_list(Module)),
+    AFunction = list_to_atom(binary_to_list(Function)),
+    try
+        Exports = ec_dictionary:get(AModule, Requires),
+        case lists:member({AFunction, PossibleArity}, Exports) of
+            true ->
+                {remote, AModule, AFunction};
+            false ->
+                invalid
+        end
+    catch
+        _:not_found ->
+            invalid
+    end.
+
+search_for_defined_used_function(Name, PossibleArity,
+                                 #context{definitions=Defs, use=Uses}) ->
+    case ec_dictionary:has_key({Name, PossibleArity}, Defs) of
+        true ->
+            apply;
+        false ->
+            case ec_dictionary:get({Name, PossibleArity}, undefined, Uses) of
+                {FunName, ModuleName} ->
+                    {remote, ModuleName, FunName};
+                undefined ->
+                    invalid
+            end
+    end.
+
+push_scope(Ctx0 = #context{scopes=Scopes}) ->
+    Ctx0#context{scopes=[sets:new() | Scopes]}.
+
+add_variable_to_scope(Name, Ctx0=#context{scopes=[Current | Scopes]}) ->
+    Ctx0#context{scopes=[sets:add_element(Name, Current) | Scopes]}.
+
+add_variables_to_scope(Names, Ctx0=#context{scopes=[Current0 | Scopes]}) ->
+   Current2 =
+        lists:foldl(fun(Name, Current1) ->
+                            sets:add_element(Name, Current1)
+                    end, Current0, Names),
+    Ctx0#context{scopes=[Current2 | Scopes]}.
+
+pop_scope(Ctx0=#context{scopes=[_|Scopes]}) ->
+    Ctx0#context{scopes=Scopes}.
